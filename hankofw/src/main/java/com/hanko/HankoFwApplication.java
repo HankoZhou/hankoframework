19853e984c549d965385ce042fe3a3a33bc6ea01
hanko <hanksandy@aliyun.com>
2021-02-18 15:14:24 +0800

    hankofw  pom.xml

---
+++ hankofw/src/main/java/com/hanko/HankoFwApplication.java
@@ -0,0 +1,13 @@
+package com.hanko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HankoFwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HankoFwApplication.class, args);
+    }
+
+}
---
+++ hankofw/src/test/java/com/hanko/HankoFwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HankoFwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
